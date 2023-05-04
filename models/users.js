1dcb1f6f464e2d0c0d46e333c5a2a77671ca2009
Olga Averianova <OlgaOlgar47@yandex.ru>
2023-05-04 09:33:27 -0300

    auth 6

--- models/users.js
+++ models/users.js
@@ -3,45 +3,48 @@ const bcrypt = require('bcryptjs');
 const validator = require('validator');
 const { default: isEmail } = require('validator/lib/isEmail');
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    validate: {
-      validator: (email) => validator.isEmail(email),
-      message: 'Некорректно введен email',
-    },
-  },
-  password: {
-    type: String,
-    required: true,
-    select: false,
-  },
-  name: {
-    type: String,
-    minlength: 2,
-    maxlength: 30,
-    default: 'Жак-Ив Кусто',
-  },
-  about: {
-    type: String,
-    minlength: 2,
-    maxlength: 30,
-    default: 'Исследователь',
-  },
-  avatar: {
-    type: String,
-    validate: {
-      validator(v) {
-        return /^(https?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w-./?%&=]*)?#?$/.test(v);
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: (email) => validator.isEmail(email),
+        message: 'Некорректно введен email',
       },
-      message: 'Некорректно введена ссылка на аватар',
     },
-    default:
-      'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    password: {
+      type: String,
+      required: true,
+      select: false,
+    },
+    name: {
+      type: String,
+      minlength: 2,
+      maxlength: 30,
+      default: 'Жак-Ив Кусто',
+    },
+    about: {
+      type: String,
+      minlength: 2,
+      maxlength: 30,
+      default: 'Исследователь',
+    },
+    avatar: {
+      type: String,
+      validate: {
+        validator(v) {
+          return /^(https?:\/\/)?([\w-]+\.)+[\w-]+(\/[\w-./?%&=]*)?#?$/.test(v);
+        },
+        message: 'Некорректно введена ссылка на аватар',
+      },
+      default:
+        'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    },
   },
-});
+  { toJSON: { useProjection: true }, toObject: { useProjection: true } }
+);
 
 // eslint-disable-next-line func-names
 userSchema.statics.findUserByCredentials = function (email, password) {
