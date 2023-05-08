9b71015307d049f58ec24e255cd72857d9743009
Olga Averianova <OlgaOlgar47@yandex.ru>
2023-05-08 20:56:51 -0300

    fix: errorHandler.js

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -7,12 +7,12 @@ const {
 const errorHandler = (err, req, res, next) => {
   const { statusCode = STATUS_INTERNAL_SERVER_ERROR } = err;
   // eslint-disable-next-line operator-linebreak
-  const message =
+  const myMessage =
     statusCode === STATUS_INTERNAL_SERVER_ERROR
       ? DEFAULT_ERROR_MESSAGE
       : err.message;
 
-  res.status(statusCode).send({ message });
+  res.status(statusCode).json({ message: myMessage });
 };
 
 module.exports = errorHandler;
