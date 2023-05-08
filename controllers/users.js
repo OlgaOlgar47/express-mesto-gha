7c21968d18214d9b9b09fa78870f74c34fa31dcc
Olga Averianova <OlgaOlgar47@yandex.ru>
2023-05-08 01:12:41 -0300

    test

--- controllers/users.js
+++ controllers/users.js
@@ -4,7 +4,13 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const User = require('../models/users');
-const SECRET_KEY = require('../config');
+const {
+  SECRET_KEY,
+  STATUS_BAD_REQUEST,
+  STATUS_NOT_FOUND,
+  STATUS_INTERNAL_SERVER_ERROR,
+  DEFAULT_ERROR_MESSAGE,
+} = require('../config');
 const BadRequestError = require('../utils/errors/BadRequestError');
 const UnauthorizedError = require('../utils/errors/UnauthorizedError');
 const NotFoundError = require('../utils/errors/NotFoundError');
@@ -140,7 +146,7 @@ const updateUser = (req, res, next) => {
     });
 };
 
-const updateAvatar = (req, res, next) => {
+const updateAvatar = (req, res) => {
   const id = req.user._id;
   const { avatar } = req.body;
   User.findByIdAndUpdate(
@@ -152,16 +158,24 @@ const updateAvatar = (req, res, next) => {
     }
   )
     .orFail(() => {
-      throw new NotFoundError();
+      throw new Error('Not found');
     })
     .then((user) => {
       res.status(200).json({ data: user });
     })
     .catch((e) => {
-      if (e.name === 'ValidationError') {
-        next(new BadRequestError());
+      const message = Object.values(e.errors)
+        .map((err) => err.message)
+        .join('; ');
+      if (e.message === 'Not found') {
+        res.status(STATUS_NOT_FOUND).json({ message: 'User not found' });
+      } else if (e.name === 'ValidationError') {
+        res.status(STATUS_BAD_REQUEST).json({ message });
+      } else {
+        res
+          .status(STATUS_INTERNAL_SERVER_ERROR)
+          .json({ message: DEFAULT_ERROR_MESSAGE });
       }
-      next();
     });
 };
 
